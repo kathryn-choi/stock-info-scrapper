b13af72e4c13bf6a5bee85c849b1fa4d3634eb64
kathryn-choi <mody135@gmail.com>
2020-03-17 12:13:38 +0900

    Modify : Scrap investor price

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Scrap stock info from naver : KOSPI, KOSDAQ, KOSPI200
 
-Scrap KOSPI, KOSDAQ, KOSPI200 from [naver finance](https://finance.naver.com/sise/)
+Scrap KOSPI, KOSDAQ, KOSPI200 info from [naver finance](https://finance.naver.com/sise/)
 <p align="center">
     <img src="https://user-images.githubusercontent.com/20381868/76387967-567fe080-63ab-11ea-800e-660f788fc145.png" />
 </p>
@@ -9,23 +9,37 @@ Scrap KOSPI, KOSDAQ, KOSPI200 from [naver finance](https://finance.naver.com/sis
 **curl -X GET 127.0.0.1:5000/api/v1/stock/index**
 ```angular2html
 {
-    "time": "2020-03-12 14:57:41.802796",
-    "KOSPI": {
-        "price": "1,830.92",
-        "fluc_price": "-77.35",
-        "fluc_rate": "-4.05"
+    "msg": "OK",
+    "data": {
+        "last_updated": "2020-03-17 12:12:13",
+        "KOSPI": {
+            "price": "1,675.38",
+            "fluc_price": "-39.48",
+            "fluc_rate": "-2.30",
+            "individual": "+2,802",
+            "foreign": "-3,912",
+            "institutional": "+933"
+        },
+        "KOSDAQ": {
+            "price": "505.88",
+            "fluc_price": "+1.37",
+            "fluc_rate": "+0.27",
+            "individual": "-2,406",
+            "foreign": "+1,800",
+            "institutional": "+594"
+        },
+        "KOSPI200": {
+            "price": "227.66",
+            "fluc_price": "-5.31",
+            "fluc_rate": "-2.28",
+            "individual": "+2,802",
+            "foreign": "-3,912",
+            "institutional": "+982"
+        }
     },
-    "KOSDAQ": {
-        "price": "566.86",
-        "fluc_price": "-28.75",
-        "fluc_rate": "-4.83"
-    },
-    "KOSPI200": {
-        "price": "247.13",
-        "fluc_price": "-9.88",
-        "fluc_rate": "-3.84"
-    }
+    "ret_code": 0
 }
+
 
 ```
 ## How to use
--- main.py
+++ main.py
@@ -14,17 +14,21 @@ def deter_incre(cur_rate):
 
 @app.route("/api/v1/stock/index")
 def scrap_info():
+
     response = requests.get("https://finance.naver.com/sise/")
     html = response.text
     soup = BeautifulSoup(html,"html.parser")
     stock_info = dict()
-    stock_info["time"] = datetime.datetime.now()
+    stock_info["last_updated"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     kospi_info = dict()
     kospi_info["price"] = soup.select("#KOSPI_now")[0].text
     kospi_fluc = soup.select("#KOSPI_change")[0].text.split("%")[0].split(" ")
     kospi_info["fluc_price"] = deter_incre(kospi_fluc[1]) + kospi_fluc[0].replace("\n","")
     kospi_info["fluc_rate"] = kospi_fluc[1]
+    kospi_info["individual"] = soup.select("#tab_sel3_deal_trend > li.c2 > span.val > em")[0].text
+    kospi_info["foreign"] = soup.select("#tab_sel3_deal_trend > li.c3 > span.val > em")[0].text
+    kospi_info["institutional"] = soup.select("#tab_sel1_deal_trend > li.c4 > a > span.val> em")[0].text
     stock_info["KOSPI"] = kospi_info
 
     kosdaq_info = dict()
@@ -32,6 +36,9 @@ def scrap_info():
     kosdaq_fluc = soup.select("#KOSDAQ_change")[0].text.split("%")[0].split(" ")
     kosdaq_info["fluc_price"] = deter_incre(kosdaq_fluc[1]) + kosdaq_fluc[0].replace("\n","")
     kosdaq_info["fluc_rate"] = kosdaq_fluc[1]
+    kosdaq_info["individual"] = soup.select("#tab_sel2_deal_trend > li.c2 > a > span.val > em")[0].text
+    kosdaq_info["foreign"] = soup.select("#tab_sel2_deal_trend > li.c3 > a > span.val > em")[0].text
+    kosdaq_info["institutional"] = soup.select("#tab_sel2_deal_trend > li.c4 > a > span.val > em")[0].text
     stock_info["KOSDAQ"] = kosdaq_info
 
     kospi200_info = dict()
@@ -39,9 +46,17 @@ def scrap_info():
     kospi200_fluc = soup.select("#KPI200_change")[0].text.split("%")[0].split(" ")
     kospi200_info["fluc_price"] = deter_incre(kospi200_fluc[1]) + kospi200_fluc[0].replace("\n","")
     kospi200_info["fluc_rate"] = kospi200_fluc[1]
+    kospi200_info["individual"] = soup.select("#tab_sel3_deal_trend > li.c2 > span.val > em")[0].text
+    kospi200_info["foreign"] = soup.select("#tab_sel3_deal_trend > li.c3 > span.val > em")[0].text
+    kospi200_info["institutional"] = soup.select("#tab_sel3_deal_trend > li.c4 > span.val > em")[0].text
     stock_info["KOSPI200"] = kospi200_info
 
-    return json.dumps(stock_info, indent=4, default=str)
+    result = dict()
+    result["msg"] = "OK"
+    result["data"] = stock_info
+    result["ret_code"] = 0
+    return json.dumps(result, indent=4)
 
 if __name__ == '__main__':
+
     app.run(debug=True)
